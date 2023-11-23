879906c5112222214e9783bc239fa9817b40f9c7
YOUNGQ <justin0701@cau.ac.kr>
2023-11-23 14:02:17 +0900

    Creating License

---
+++ convert_bvh_fbx.py
@@ -0,0 +1,31 @@
+import bpy
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bvh_path',type=str,default="./.bvh") #Your bvh
+parser.add_argument('--fbx_path',type=str,default="./.fbx") #Export fbx
+
+args = parser.parse_args()
+
+# 씬 셋팅
+bpy.ops.wm.read_factory_settings(use_empty=True)
+
+# Bvh file
+bpy.ops.import_anim.bvh(filepath=args.bvh_path)
+
+# Export to FBX
+bpy.ops.export_scene.fbx(filepath=args.fbx_path, use_selection=True)
+
+scene = bpy.context.scene
+for obj in scene.objects:
+    if obj.type == 'ARMATURE':
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.armature.select_all(action='DESELECT')
+        bpy.ops.armature.select_all(action='SELECT')
+        bpy.ops.armature.delete(type='PREVIOUS')
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+bpy.ops.wm.link_remove(1) 
+
+
